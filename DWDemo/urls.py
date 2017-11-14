3359bfdc113db0a817ebfd9fb6e64c3d53dd8e44
a.gorbachev@digitalwand.ru <a.gorbachev@digitalwand.ru>
2017-11-14 20:26:25 +0500

    Frontend::Auth page

--- DWDemo/urls.py
+++ DWDemo/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
 
 admin.autodiscover()
 
 urlpatterns = [
+    url(r'^login/$', auth_views.login, {"template_name": "registration/login.html"}, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^admin/', admin.site.urls),
 ]
